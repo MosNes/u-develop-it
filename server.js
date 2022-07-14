f83daa64f06e1f42f0e69b3d405b54e692e866b1
MosNes <7hekarl@gmail.com>
2022-07-13 20:16:48 -0400

    added DB connection to server.js, tested using a db.query()

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //-----DEPENDENCIES AND GLOBAL VARIABLES-----------------------------------------
 
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const secrets = require('./secrets.json');
 
 
 const app = express();
@@ -11,9 +13,26 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MYSQL UN
+        user: 'root',
+        //your MYSQL PW
+        password: secrets.db_pw,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 //------ROUTES--------------------------------------------------------------------
 
-//default response for any other request (not Found)
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+//default catch-all response for any other request (not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
