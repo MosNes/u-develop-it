585efdd40d0754749922c3559c8443e6aaf333aa
MosNes <7hekarl@gmail.com>
2022-07-17 14:15:43 -0400

    added route to update candidate party

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.delete('/api/candidates/:id', (req, res) => {
 
     db.query(sql, params, (err, result) => {
         if (err) {
-            res.status(400).json({ error: res.message });
+            res.status(400).json({ error: err.message });
         } else if (!result.affectedRows) {
             res.json({
                 message: 'Candidate not found'
@@ -115,6 +115,37 @@ app.post('/api/candidate', ({ body }, res) => {
             message: 'success',
             data: body
         });
+    });
+});
+
+//update a single candidate's party
+app.put('/api/candidates/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `UPDATE candidates SET party_id = ?
+                WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            // check if record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            })
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
     });
 });
 
